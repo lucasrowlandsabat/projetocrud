3825ee34777a8bbf027d9341f14f4829b5801fed
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-19 09:55:10 -0300

    Adicionando funcoes e estilizando a página

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,41 @@ const express = require("express"); //IMPORTA O MÓDULO EXPRESSO PARA CONSTRUIR 
 const cors = require("cors"); //PERMITE QUE ACESSE ROTAS DIFERENTES(DOMÍNIOS)
 const bodyParser = require("body-parser");// MIDDLEWARE QUE ANALISA OS DADOS DO CORPO DA PÁGINA DA REQUISIÇÃO(EX. DADOS QUE VEM DO FORMULARIOS)
 const {v4:uuid} = require("uuid");//FUNÇÃO RESPONSAVEL POR GERAR OS CÓDIGOS ÚNICOS
+const path = require("path"); // CAMINHO DOS DADOS DO SEU PROJETO
+const fs = require("fs"); //MANIPULA OS ARQUIVOS DO JSON
 
 const app = express(); // INSTANCIANDO O EXPRESS
 const port = 5001; //DEFINE A PORTA DO SERVIDOR
 app.use(cors()); //HABILITA O CORS PARA ACEITAR AS REQUISIÇÕES 
 app.use(bodyParser.json()); //USANDO O BODY-PARSER PARA RECEBER AS REQUISIÇÕES E CONVERTER PARA JSON NO CORPO DA PÁGINA
 
-let produtos =[]; //VARIAVEL QEU RECEBE UM ARRAY VAZIO;
+// LOCAL AONDE ESTA O ARQUIVO DO SEU BANCO DE DADOS
+const caminho = path.join(__dirname,"produtos.json");
+
+// FUNÇÃO PARA LER OS DADOS DO ARQUIVO JSON
+const lerProdutos =()=>{
+    try{
+        const data = fs.readFileSync(caminho, "utf-8")
+        return JSON.parse(data);
+    }catch(error){
+        console.error("Erro ao ler o arquivos produtos.json",error)
+        return [];
+
+    }
+}
+
+// FUNÇÃO PARA GRAVAR OS DADOS NO ARQUIVO JSON
+const salvarProdutos =(data)=>{
+    try{
+        fs.writeFileSync(caminho, JSON.stringify(data,null,2),"utf-8")
+    }catch(error){
+        console.error("Erro ao salvaf dados no arquivo",error)
+    }
+}
+
+// VARIAVEL QUE RECEBE OS DADOS DO ARQUIVO 
+let produtos =lerProdutos();
+
 
 
 // CRIANDO A ROTA CADASTRAR PRODUTO (post)
@@ -23,6 +51,8 @@ app.post("/produto",(req,res)=>{
     const novoItem ={id:uuid(),nome, descricao}
     // PEGA O QUE FOI CADASTRADO E ADICIONA NO ARRAY PRODUTOS
     produtos.push(novoItem);
+    // SALVA OS DADOS NO ARQUIVO JSON
+    salvarProdutos(produtos)
     // RETORNA A MENSAGEM DE SUCESSO
     res.status(201).json({message: "Cadastro efetuado com sucesso"})
 })
@@ -30,7 +60,27 @@ app.post("/produto",(req,res)=>{
 // CRIANDO A ROTA CONSULTAR PRODUTOS (get)
 app.get("/produto",(req,res)=>{
     res.json(produtos);
+});
+
+// CRIANDO A ROTA PARA CONSULTA PERSONALIZADA (get)
+
+app.get("/produto/search",(req,res)=>{
+    // //OBTÉM O PARAMETRO DE PESQUISA NA URL
+    const { pesquisa } = req.query;
+    // VALIDANDO PESQUISA
+    if(!pesquisa){
+        return res.status(400).json({error:"Pesquisa não encontrada"})
+    }
+    // CONVERT O TERMO DE PESQUISA PARA MINUSCULAS
+    const termoPesquisa = pesquisa.toLowerCase();
+    // FILTRA OS PRODUTOS QUE CONTEM O TERMO DE PESQUISA NO NOME OU DESCRICAO
+    const resultado = produtos.filter(item =>item.nome.toLowerCase().includes(termoPesquisa) || item.descricao.toLowerCase().includes(termoPesquisa)
+    );
+    res.json(resultado)
 })
+
+
+
 
 // CRIANDO A ROTA ALTERAR PRODUTOS (put)
 app.put("/produto/:id", (req,res)=>{
@@ -50,6 +100,8 @@ app.put("/produto/:id", (req,res)=>{
     // CONFIRMA A ALTERAÇÃO DO PRODUTO
     produtos[produtoIndex]={id:produtoId,nome,descricao};
     // RETORNA NO ARRAY COM A ALTERAÇÃO FEITA
+    // SALVA AS ALTERAÇÃOES NO ARQUIVO JSON
+    salvarProdutos(produtos)
     res.json(produtos[produtoIndex])
 })
 
@@ -69,15 +121,10 @@ app.delete("/produto/:id", (req,res)=>{
     if(produtos.length == inicioProduto){
         return res.status(404).json({error:"Produto não encontrado"})
     }
+    // SALVA AS ALTERAÇÕES DE DELETAR NO ARQUIVO JSON
+    salvarProdutos(produtos)
     // MENSAGEM QUE CONFIRMA QUE O PRODUTO FOI REMOVIDO
     res.status(201).send("Produto removido com sucesso")
-})
-
-
-
-// EXECUTANDO A ESCUTA DO SERVIDOR
-app.listen(port, ()=>{
-    console.log(`Servidor rodando na porta http://localhost:${port}`);
 })
 
 
--- frontend/src/components/Produto.jsx
+++ frontend/src/components/Produto.jsx
@@ -1,119 +1,137 @@
-import{ useState, useEffect} from "react"
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Produto = () => {
+  // DECLARANDO A URL DA API QUE SERÁ CONSUMIDA
+  const API_URL = "http://localhost:5001/produto";
 
-// DECLARANDO A URL DA API QUE SERÁ CONSUMIDA
-const API_URL ="http://localhost:5001/produto";
+  // HOOK- MANIPULA O ESTADO DA VARIÁVEL
+  const [produto, setProduto] = useState([]);
+  const [novoProduto, setNovoProduto] = useState({ nome: "", descricao: "" });
+  const [editar, setEditar] = useState(false);
+  const [pesquisar,setPesquisar]= useState("");
 
-// HOOK- MANIPULA O ESTADO DA VARIÁVEL
-const [produto,setProduto]=useState([]);
-const [novoProduto,setNovoProduto]=useState({nome:"",descricao:""});
-const [editar,setEditar]=useState(false);
-
-// CADASTRAR PRODUTO (POST)
-//função assincrona para cadastrar um novo produto, 
-const cadastrarProduto =async ()=>{
+  // CADASTRAR PRODUTO (POST)
+  //função assincrona para cadastrar um novo produto,
+  const cadastrarProduto = async() => {
     // validar campos
-    if(!novoProduto.nome || !novoProduto.descricao){
-        alert("Campos Obrigatórios")
-        return;
+    if (!novoProduto.nome || !novoProduto.descricao) {
+      alert("Campos Obrigatórios");
+      return;
     }
     // TRATAMENTO DE ERROS
-    try{
-        // envia os dados do novoProduto para api usando uma requisição POST
-        const response = await axios.post(API_URL,novoProduto);
-        // se a requisição for bem-sucedida, atualiza o estado do produto
-        setProduto([...produto, response.data]);
-        // limpa os campos do formulario redefinindo o estado novoProduto
-        setNovoProduto({nome:"", descricao:""})
-        // define o estado editar como false indicando que a botão editar não esta ativo
-        setEditar(false);
-    }catch(error){
-        console.log("erro ao cadatrar o protudo",error)
+    try {
+      // envia os dados do novoProduto para api usando uma requisição POST
+      const response = await axios.post(API_URL, novoProduto);
+      // se a requisição for bem-sucedida, atualiza o estado do produto
+      setProduto([...produto, response.data]);
+      // limpa os campos do formulario redefinindo o estado novoProduto
+      setNovoProduto({ nome: "", descricao: "" });
+      // define o estado editar como false indicando que a botão editar não esta ativo
+      setEditar(false);
+    } catch (error) {
+      console.log("erro ao cadatrar o protudo", error);
     }
-}
+  };
 
-// CONSULTANDO PRODUTOS (GET)
+  // CONSULTANDO PRODUTOS (GET)
 
-const consultarProdutos =async()=>{
-    try{
-        const response = await axios.get(API_URL);
-        setProduto(response.data)
+  const consultarProdutos = async()=> {
+    try {
+      //VERIFICA SE TROUXER UMA PESQUISA ESPECIFICA SENÃO DEVOLVE A LISTA COM TODOS 
+      const url = pesquisar ? `${API_URL}/search?pesquisa=${pesquisar}`: API_URL;
+      const response = await axios.get(url);
+      setProduto(response.data);
 
-    }catch(error){
-        console.log("error ao consultar produto", error)
+    } catch (error) {
+      console.log("error ao consultar produto", error);
     }
-}
+  };
 
-// HOOK - useEffect - efeito apra carregar a lista de todos os produtos cadastrados
+  // HOOK - useEffect - efeito apra carregar a lista de todos os produtos cadastrados
 
-useEffect(()=>{
-    consultarProdutos();
-})
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+       consultarProdutos();
+    },300) //resposta em 3 segundos
+    return ()=> clearTimeout(timer)
+  },[pesquisar]);
 
-// ALTERANDO PRODUTOS (PUT)
+  // ALTERANDO PRODUTOS (PUT)
 
-const alterarProduto =async()=>{
-    if(!novoProduto.nome || novoProduto.descricao){
-        alert("Campos obrigatórios")
-        return;
+  const alterarProduto = async () => {
+    if (!novoProduto.nome || !novoProduto.descricao) {
+      alert("Campos obrigatórios");
+      return;
     }
     // TRATAMENTO DE ERROS
-    try{
-        // Envia uma requisição PUT para api , a url adiciona o ID do produto a ser alterado
-        //o objeto novoProduto contem os dado atualizados para requisição
-        const response = await axios.put(`${API_URL}/${novoProduto.id}`,novoProduto);
-        //se a requisição for bem-sucedida, atualizacao estado do produto
-        //mapeia a lista de produtos,substituindo o item com o id correspondente pelos dados retornos pela apli(response.data)
-        setProduto(produto.map(produto =>produto.id === novoProduto.id ? response.data: produto))
-        setNovoProduto({nome:"",descricao:""})
-        //define o estado do editar como false indicando que a edição foi concluida
-        setEditar(false)
+    try {
+      // Envia uma requisição PUT para api , a url adiciona o ID do produto a ser alterado
+      //o objeto novoProduto contem os dado atualizados para requisição
+      const response = await axios.put(`${API_URL}/${novoProduto.id}`,novoProduto
+      );
+      // se a requisição for bem sucedidaa , atualizao estado do produto
+      //mapeia a lista de produtos,substituindo o item com o id correspondente pelos dados retornos pela apli(response.data)
+      setProduto(
+        produto.map((produto) =>
+          produto.id === novoProduto.id ? response.data : produto
+        )
+      );
+      // limpa os campos do formulario
+      setNovoProduto({ nome: "", descricao: "" });
+      // define o estado do editar como false indicando que a edição foi concluida
+      setEditar(false);
+    } catch (error) {
+      console.log("erro ao alterar produto", error);
+    }
+  };
 
-    }catch(error){
-        console.log("erro ao alterar produto",error)
+  // DELETAR PRODUTO (DELETE)
+  const deletarProduto = async (id) => {
+    if (window.confirm("Tem certeza que deseja deletar este produto"))
+      try {
+        // envia uma requisição delete para a api usando o id do produto na url
+        await axios.delete(`${API_URL}/${id}`);
+        // filtra a lista de produto removendo o produto que possui o id correspondente
+        setProduto(produto.filter((produto)=>produto.id !== id));
+      } catch (error) {
+        console.log("Erro ao deletar produto", error);
+      }
+    else {
+      console.log("Exlusão de produto cancelada");
     }
-}
-
-// DELETAR PRODUTO (DELETE)
-const deletarProduto  =async(id)=>{
-    if(window.confirm("Tem certeza que deseja deletar este produto"))
-        try{
-    // envia uma requisição delete para a api usando o id do produto na url
-        await axios.delete(`${API_URL}/${id}`)
-    // filtra a lista de produto removendo o produto que possui o id correspondente
-        setProduto(produto.filter((produto)=> produto.id !== id));
-    }catch(error){
-        console.log("Erro ao deletar produto",error)
-    }
-    else{
-        console.log("Exlusão de produto cancelada")
-    }
-}
-// METODO ALTERAR
-const handleAlterar= (produto)=>{
+  };
+  // MÉTODO ALTERAR
+  const handleAterar=(produto)=> {
     setNovoProduto(produto);
-    setEditar(true)
-}
+    setEditar(true);
+  };
 
-// METODO SUBMIT QUE VAI ATUALIZAR O BOTÃO NO FORMULARIO
+  // METODO SUBMIT QUE VAI ATUALIZAR O BOTÃO NO FORMULARIO
 
-const handleSubmit=()=>{
-    if(editar){
-        alterarProduto();
-    }else{
-        cadastrarProduto();
+  const handleSubimit = () => {
+    if (editar) {
+      alterarProduto();
+    } else {
+      cadastrarProduto();
     }
-}
-
+  };
 
   return (
-    <div>
-      <h1>Cadastro de Produto</h1>
-      <form>
+    <div className="mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-8 text-center">Cadastro de Produto</h1>
+      <form className="mb-8">
         <div>
-          <label>Nome do Produto</label>
+          <input
+            type="text"
+            placeholder="Pesquisar Produtos..."
+            value={pesquisar}
+            onChange={(e)=>setPesquisar(e.target.value)}
+            className="w-[300px] pl-4 pr-4 py-2 border border-gray-500 rounded-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-xl font-medium text-gray-700">Nome do Produto</label>
           <input
             type="text"
             id="nome"
@@ -127,10 +145,11 @@ const handleSubmit=()=>{
             onChange={(e) =>
               setNovoProduto({ ...novoProduto, nome: e.target.value })
             }
+            className="mt-4 rounded border w-[400px]"
           />
         </div>
         <div>
-          <label>Descrição do Produto</label>
+          <label className="block text-xl font-medium text-gray-700">Descrição do Produto</label>
           <input
             type="text"
             id="descricao"
@@ -139,22 +158,30 @@ const handleSubmit=()=>{
             onChange={(e) =>
               setNovoProduto({ ...novoProduto, descricao: e.target.value })
             }
+            className="mt-4 rounded border w-[400px]"
           />
         </div>
 
-        <button onClick={handleSubmit}>
+        <button onClick={handleSubimit}
+          className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 mt-4 rounded"
+        >
           {editar ? "Alterar" : "Cadastrar"}
         </button>
       </form>
       <ul>
         {produto.map(produto => (
-          <li key={produto.id}>
+          <li key={produto.id} className="border p-4 mb-4 rounded flex items-center justify-between">
             <div>
               <strong>{produto.nome}</strong>{produto.descricao}
             </div>
             <div>
-              <button onClick={()=>handleAlterar(produto)}>Editar</button>
-              <button onClick={()=>deletarProduto(produto.id)}>Deletar</button>
+              <button onClick={()=>handleAterar(produto)}
+              className="bg-amber-300 hover:bg-amber-500 text-black font-bold py-4 px-4 rounded mr-4"
+                >Editar
+                </button>
+              <button onClick={()=>deletarProduto(produto.id)}
+                className="bg-red-300 hover:bg-red-500 text-black font-bold py-4 px-4 rounded mr-4"
+                >Deletar</button>
             </div>
           </li>
         ))}
